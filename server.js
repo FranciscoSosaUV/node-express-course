91b55c594e0d1abbd2061fe19547b1a27ac34d63
Francisco Sosa <fsosa@unitedvirtualities.com>
2019-12-02 10:46:48 -0700

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express'); //da acceso a la biblioteca express buscando en sus node_modules "express"
 const app = express(); // crea una instancia del constructor express, al que llamaremos "aplicación".
 
+const mockUserData=[ //JSON
+    {name:'Mark'},
+    {name:'Jill'}
+    ]
+    app.get('/users', function(req,res){ //GET 
+         res.json({
+              success: true,
+              message: 'successfully got users. Nice!',
+              users: mockUserData
+         })
+    })
 
 app.listen(8000,function(){
     console.log("server is running")  // iniciará el servidor localmente en el puerto que proporcione como primer argumento 
