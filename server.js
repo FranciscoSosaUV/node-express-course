ee1187fe2963b3a6004866c5354a62e64100a073
Francisco Sosa <fsosa@unitedvirtualities.com>
2019-12-02 10:57:36 -0700

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express'); //da acceso a la biblioteca express buscando en sus node_modules "express"
 const app = express(); // crea una instancia del constructor express, al que llamaremos "aplicación".
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+
 const mockUserData=[ //JSON
     {name:'Mark'},
     {name:'Jill'}
@@ -21,6 +26,27 @@ const mockUserData=[ //JSON
             user: req.params.id
         })
     })
+
+    app.post('/login',function(req,res){ //POST login example
+        const username=req.body.username;
+        const password=req.body.password;
+    
+        const mockUsername="billyTheKid";
+        const mockPassword="superSecret";
+    
+        if (username===mockUsername && password===mockPassword){
+             res.json({
+                  success: true,
+                  message: 'password and username match!',
+                  token: 'encrypted token goes here'
+             })
+        } else {
+             res.json({
+                  success: false,
+                  message: 'password and username do not match'
+             })
+        }
+   })
 
 
 app.listen(8000,function(){
